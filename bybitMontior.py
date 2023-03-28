5137377b37e3f6a3ab37ec089bedb0aa930e6aa3
Eric <ericlauchiho@gmail.com>
2023-03-28 18:41:49 +0900

    remove socket.ping, only keeping socket.send

--- bybitMontior.py
+++ bybitMontior.py
@@ -58,10 +58,9 @@ class CustomWebSocketApp(websocket.WebSocketApp):
             if self.sock:
                 self.last_ping_tm = time.time()
                 try:
-                    self.sock.ping("")
                     self.sock.send(self.ping_payload)
                 except Exception as ex:
-                    logging.debug("Failed to send ping: %s", ex)
+                    websocket._logging.debug("Failed to send ping: %s", ex)
 
 if __name__ == "__main__":
     ping_body = json.dumps({
