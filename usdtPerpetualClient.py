65aa2b2ae01bed9333acf044b2992af6efa46c03
Eric <ericlauchiho@gmail.com>
2023-04-02 16:19:52 +0900

    update ping frequency

---
+++ usdtPerpetualClient.py
@@ -0,0 +1,8 @@
+from pybit import usdt_perpetual
+import os
+
+trade_api_key =  os.environ['BYBIT_TRADE_API_KEY']
+trade_api_secret = os.environ['BYBIT_TRADE_API_SECRET']
+
+if __name__ == "__main__":
+    print('test')
