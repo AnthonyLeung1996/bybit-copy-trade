f7b5f2ab59862a4f8ab30a0df22b6f7f94acd01c
Eric <ericlauchiho@gmail.com>
2023-04-09 10:25:18 +0900

    fix ping

--- bybitCopyTrade.py
+++ bybitCopyTrade.py
@@ -90,6 +90,7 @@ class CustomWebSocketApp(websocket.WebSocketApp):
             if self.sock:
                 self.last_ping_tm = time.time()
                 try:
+                    self.sock.ping('')
                     self.sock.send(self.ping_payload)
                 except Exception as ex:
                     websocket._logging.error("Failed to send ping: %s", ex)
