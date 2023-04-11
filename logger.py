3c197318c304bbb1cfb3c65de3a00d5a0a1cb8c2
Eric <ericlauchiho@gmail.com>
2023-04-11 13:38:32 +0900

    also save log to file

--- logger.py
+++ logger.py
@@ -6,11 +6,13 @@ def timetz(*args):
     return datetime.now(timezone('Asia/Tokyo')).timetuple()
 
 logging.Formatter.converter = timetz
-
 logging.basicConfig(
     format='[%(asctime)s][%(name)s][%(levelname)s]: %(message)s',
     level=logging.INFO,
-    datefmt='%Y-%m-%d %H:%M:%S'
+    datefmt='%Y-%m-%d %H:%M:%S',
+    handlers=[
+        logging.FileHandler('/logs/{:%Y-%m-%d_%H:%M:%S}.log'.format(datetime.now())),
+        logging.StreamHandler()
+    ]
 )
-
 logger = logging.getLogger()
