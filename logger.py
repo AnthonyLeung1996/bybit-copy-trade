0474090eff6c2cb15463f991f4b133dac45cd6c7
Eric <ericlauchiho@gmail.com>
2023-10-12 12:23:27 +0900

    improve date format

--- logger.py
+++ logger.py
@@ -9,7 +9,7 @@ logging.Formatter.converter = timetz
 logging.basicConfig(
     format='[%(asctime)s][%(name)s][%(levelname)s]: %(message)s',
     level=logging.INFO,
-    datefmt='%Y-%m-%d %H-%M-%S',
+    datefmt='%Y-%m-%d %H:%M:%S',
     handlers=[
         logging.FileHandler('/logs/{:%Y-%m-%d_%H-%M-%S}.log'.format(datetime.now())),
         logging.StreamHandler()
