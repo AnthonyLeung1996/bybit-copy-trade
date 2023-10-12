30d55a731adf6de6a9c6ad53fb4a6d39c0479e23
Eric <ericlauchiho@gmail.com>
2023-10-12 18:03:05 +0900

    refactor reporter, improve logging

--- taskScheduler.py
+++ taskScheduler.py
@@ -1,13 +1,13 @@
 import time, threading
 from typing import Callable
 
-class BalanceReporter:
+class TaskScheduler:
     def __init__(self, intervalInSeconds: float, actionFunction: Callable) :
         self.intervalInSeconds = intervalInSeconds
         self.actionFunction = actionFunction
         self.stopEvent = threading.Event()
     
-    def __reportBalanceWorkerFunction(self) :
+    def __workerFunction(self) :
         nextTime = time.time() + self.intervalInSeconds
         while not self.stopEvent.wait(nextTime - time.time()) :
             nextTime += self.intervalInSeconds
@@ -15,7 +15,7 @@ class BalanceReporter:
         
     def start(self):
         self.actionFunction()
-        thread = threading.Thread(target = self.__reportBalanceWorkerFunction)
+        thread = threading.Thread(target = self.__workerFunction)
         thread.start()
 
     def cancel(self):
